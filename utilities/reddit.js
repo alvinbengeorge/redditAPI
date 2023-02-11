bdc264ac0ab6a8377d5d8272611a265dd3f11bed
Alvin <alvinallen333@gmail.com>
2023-02-11 13:17:50 +0530

    Added ? for safety purposes

--- utilities/reddit.js
+++ utilities/reddit.js
@@ -1,9 +1,9 @@
 async function getMemes(subreddit, index = Math.floor(Math.random() * 25)) {
     const response = await fetch(`https://reddit.com/r/${subreddit}.json`);
     const data = await response.json();
-    return data.data.children.map(({ data }) => ({
+    return data?.data?.children.map(({ data }) => ({
         title: data.title,
         url: data.url,
-    }))[index];
+    }))[index] || {title: "No memes found", url: ""};
 }
 export default getMemes;
