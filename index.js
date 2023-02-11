d7915e841bd68e4612e0a09cbc1015d7b87bea5c
Alvin <alvinallen333@gmail.com>
2023-02-11 13:23:12 +0530

    Added 404 page and with index

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get("/meme/:subreddit", async function(req, res) {
     res.send(await getMemes(req.params.subreddit));
 });
 
+app.get("*", (req, res) => {
+    res.send({"message": "404 Not Found"});
+})
+
+app.get("/meme/:subreddit/:index", async function(req, res) {
+    res.send(await getMemes(req.params.subreddit, req.params.index));
+});
+
 app.listen("8000", () => {
   console.log("Server is running on port 8000");
 })
